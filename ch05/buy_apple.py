db5409f10782a40dd59e3e69172c593671efbe2f
flekystyley <flekystyley@gmail.com>
2020-03-11 15:14:12 +0900

    update buy_apple;

--- ch05/buy_apple.py
+++ ch05/buy_apple.py
@@ -8,9 +8,11 @@ tax = 1.1
 mul_apple_layer = MulLayer()
 mul_tax_layer = MulLayer()
 
+# forward
 apple_price = mul_apple_layer.forward(apple, apple_num)
 price = mul_tax_layer.forward(apple_price, tax)
 
+# backword
 dprice = 1
 dapple_price, dtax = mul_tax_layer.backword(dprice)
 dapple, dapple_num = mul_apple_layer.backword(dapple_price)
